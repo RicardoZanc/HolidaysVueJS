8ea6dd64b79631cc9070b8127f0163f56a88e62f
Ricardo <ricardozancacn@gmail.com>
2024-12-08 18:04:15 -0300

    frontend started

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
-  <h2 class="text-3xl font-bold underline">Hello</h2>
+
+<header>
+    <nav class="bg-black">
+    <div class="max-w-screen-xl flex mx-auto p-4">
+      <h1 class="self-center text-2xl font-semibold whitespace-nowra text-white">HolidaySearch</h1>
+    </div>
+    </nav>
+  </header>
+  <main class="w-full h-full d-flex flex grow items-center justify-center ">
+    <SearchCard/>
+  </main>
 </template>
+
+  <script setup>
+  import SearchCard from "@/components/SearchCard.vue"
+  </script>
---
+++ src/components/SearchCard.vue
@@ -0,0 +1,18 @@
+<template>
+
+
+<div class="w-8/12 h-4/6 p-6  rounded">
+    <div class="bg-black rounded px-6 py-8">
+        <h1 class="text-center self-center text-2xl font-semibold whitespace-nowra text-white">Pesquisar ano</h1>
+    </div>
+    <div class="bg-white h-0">
+        <p>breve descrição</p>
+        <p>data</p>
+    </div>
+</div>
+
+</template>
+
+<script setup>
+
+</script>
